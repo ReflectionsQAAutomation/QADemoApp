4152cb3bf688a9002913e67d2aea79f405db1df2
Jothish Johny <jothish.johny@reflectionsinfos.com>
2024-08-13 16:11:04 +0530

    update login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,21 @@
 "use client"
 
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
 import { useRouter } from 'next/navigation'
+import { lusitana } from '@/app/ui/fonts';
+import {
+    AtSymbolIcon,
+    KeyIcon,
+    ExclamationCircleIcon,
+} from '@heroicons/react/24/outline';
+import { ArrowRightIcon } from '@heroicons/react/20/solid';
+import { Button } from '@/app/ui/button';
 
 export default function LoginPage() {
 
     const router = useRouter()
 
-    let errorMessage: string = ''
+    const [errorMessage, setErrorMessage] = useState<string>('')
 
     async function handleSubmit(event: FormEvent<HTMLFormElement>) {
 
@@ -25,51 +33,76 @@ export default function LoginPage() {
             })
 
             if (response.ok) {
-
                 router.push('/dashboard')
             } else {
-            
-                errorMessage = 'invalid username or password'
+                setErrorMessage('Invalid username or password')
             }
         } catch (error) {
-
-            errorMessage = 'invalid username or password'
+            setErrorMessage('Something went wrong. Please try again.')
         }
     }
 
     return (
         <div className="max-w-md mx-auto mt-20">
             <div className="rounded-md bg-gray-50 p-4 md:p-6">
-                <h1 className="text-2xl font-bold mb-4">Login</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="mb-2 block text-sm font-medium">Email</label>
-                        <input
-                            id='email'
-                            name='email'
-                            type="email"
-                            placeholder="Enter email"
-                            defaultValue='eve.holt@reqres.in'
-                            onChange={(e) => e.target.value}
-                            className="peer block w-full rounded-md border border-gray-200 py-[9px] text-sm outline-2 placeholder:text-gray-500"
-                        />
+                <form className="space-y-3" onSubmit={handleSubmit}>
+                    <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+                        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+                            Please log in to continue.
+                        </h1>
+                        <div className="w-full">
+                            <div>
+                                <label
+                                    className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                                    htmlFor="email"
+                                >
+                                    Email
+                                </label>
+                                <div className="relative">
+                                    <input
+                                        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        placeholder="Enter your email address"
+                                        defaultValue='eve.holt@reqres.in'
+                                        required
+                                    />
+                                    <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                                </div>
+                            </div>
+                            <div className="mt-4">
+                                <label
+                                    className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                                    htmlFor="password"
+                                >
+                                    Password
+                                </label>
+                                <div className="relative">
+                                    <input
+                                        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        placeholder="Enter password"
+                                        required
+                                        minLength={6}
+                                        defaultValue='cityslicka'
+                                    />
+                                    <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                                </div>
+                            </div>
+                        </div>
+                        <Button className="mt-4 w-full" type='submit'>
+                            Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+                        </Button>
+                        {errorMessage && (
+                            <div className="mt-4 flex items-center text-red-600">
+                                <ExclamationCircleIcon className="h-5 w-5 mr-2" />
+                                <span>{errorMessage}</span>
+                            </div>
+                        )}
                     </div>
-                    <div className="mb-4">
-                        <label className="mb-2 block text-sm font-medium">Password</label>
-                        <input
-                            id='password'
-                            name='password'
-                            type="password"
-                            placeholder="Enter password"
-                            defaultValue='cityslicka'
-                            onChange={(e) => e.target.value}
-                            className="peer block w-full rounded-md border border-gray-200 py-[9px] text-sm outline-2 placeholder:text-gray-500"
-                        />
-                    </div>
-                    <p>{errorMessage}</p>
-                    <button type="submit" className="h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50 w-full text-center">
-                        Login
-                    </button>
                 </form>
             </div>
         </div>
